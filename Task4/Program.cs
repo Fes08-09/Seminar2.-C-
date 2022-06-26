3f7dc38f757aeae377e49b33e5869ccbeff0f802
Dmitriy <Fes08-09@yandex.ru>
2022-06-26 22:50:01 +0300

    Task 4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+using System;
+using static System.Console;
+
+Clear();
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+
+int a = number/10;
+int a1 = a%10;
+WriteLine(a1);
+
