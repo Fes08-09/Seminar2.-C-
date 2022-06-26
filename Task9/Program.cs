c87aeb9884657a04d77792df679f30b7c9105f60
Dmitriy <Fes08-09@yandex.ru>
2022-06-26 22:53:03 +0300

    Task 9 done

---
+++ Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int number = new Random().Next(10, 100);
+WriteLine($"Случайное число = {number}"); // для подписи при выводе данных. $ - означает {} - служебные символы
+
+int a1 = number/10;
+int a2 = number%10;
+
+if(a1 > a2)
+{
+    WriteLine($"Максимальное число = {a1}");
+}
+else
+{
+    WriteLine($"Максимальное число = {a2}");
+}
+
+
